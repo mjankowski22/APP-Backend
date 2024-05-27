73126cf3c886d7e436382b9a09c9a0709791fe80
Michal Jankowski <s188831@student.pg.edu.pl>
2024-05-27 22:01:54 +0200

    dodanie endpointow

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,jsonify
 from flask_mqtt import Mqtt
 import json
 import base64
 from datetime import datetime
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_socketio import SocketIO,emit
 
 
 
 app = Flask(__name__)
+socketIO = SocketIO(app)
+
+
+@SocketIO.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@SocketIO.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
 
 
 ## Nie ruszać
@@ -59,7 +69,33 @@ class WeatherData(db.Model):
 
 
 
+@app.route('/lora_dane/all', methods=['POST','GET'])
+def lora_dane():
+    data = Data.query.all()
+    data_list = [{
+        'id': d.id,
+        'timestamp': d.timestamp,
+        'latitude': d.latitude,
+        'longitude': d.longitude,
+        'memory_status': d.memory_status
+    } for d in data]
+    return jsonify(data_list)
 
+
+@app.route('/5g_dane/all', methods=['POST','GET'])
+def fiveg_dane():
+    data = WeatherData.query.all()
+    data_list = [{
+        'id': d.id,
+        'date_and_time': d.date_and_time.strftime('%Y-%m-%d %H:%M:%S'),
+        'temperature_inside': d.temperature_inside,
+        'atmospheric_pressure': d.atmospheric_pressure,
+        'light_intensity': d.light_intensity,
+        'water_temperature': d.water_temperature,
+        'localization_n': d.localization_n,
+        'localization_e': d.localization_e
+    } for d in data]
+    return jsonify(data_list)
 
 
 @app.route('/upload_file', methods=['POST'])
@@ -101,80 +137,43 @@ def check_5g():
 
 @app.route('/upload5g_whole_request', methods=['POST'])
 def request_whole_file_5g():
-    py_dict = {'5g_send_whole':1}
-    message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
-    mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
-    #console_messages.append(f'<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Requesting 5G send whole file')
+    
+    data = request.get_json()
+    if 'delete' in data:
+        try:
+            delete = data['delete']
 
-    return redirect('/')
+            py_dict = {'5g_send_whole':1,'delete':delete}
+            message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
+            mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
+
+            return 200
+        
+        except ValueError as e:
+            return 400
+    else:
+        return 400
+    
 
 @app.route('/upload5g_part_request', methods=['POST'])
 def request_part_file_5g():
-    start_date = request.form['start_date']
-    end_date = request.form['end_date']
+    data = request.get_json()
+    if 'start_date' in data and 'end_date' in data and 'delete' in data:
+        try:
+            start_date = str(datetime.strptime(data['start_date'], '%Y-%m-%d'))
+            end_date = str(datetime.strptime(data['end_date'], '%Y-%m-%d'))
+            delete = data['delete']
 
-    start_datetime = str(datetime.strptime(start_date, "%Y-%m-%d"))
-    end_datetime = str(datetime.strptime(end_date, "%Y-%m-%d"))
-
-    py_dict = {'5g_send_part':1,'start':start_datetime,'end':end_datetime}
-    message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
-    mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
-    #console_messages.append(f'<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Requesting 5G send part file {start_datetime}-{end_datetime}')
-    
-    return redirect('/')
-
-
-
-### Cała logika z wifi do wywalenia - będzie nowa
-@app.route('/check-wifi', methods=['POST'])
-def check_wifi():
-    py_dict = {'wifi_check':1}
-    message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
-    mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
-    #console_messages.append(f'<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Checking WiFi connection..')
-
-    return redirect('/')
-
-@app.route('/uploadwifi_whole_request', methods=['POST'])
-def request_whole_file_wifi():
-    py_dict = {'wifi_send_whole':1}
-    message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
-    mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
-    #console_messages.append(f'<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Requesting WiFi send whole file')
-
-    return redirect('/')
+            py_dict = {'5g_send_part':1,'start':start_date,'end':end_date,'delete':delete}
+            message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
+            mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
+            return 200
+        except ValueError as e:
+            return 400
+    else:
+        return 400
 
 
-@app.route('/uploadwifi_part_request', methods=['POST'])
-def request_part_file_wifi():
-    start_date = request.form['start_date']
-    end_date = request.form['end_date']
-
-    start_datetime = str(datetime.strptime(start_date, "%Y-%m-%d"))
-    end_datetime = str(datetime.strptime(end_date, "%Y-%m-%d"))
-
-    py_dict = {'wifi_send_part':1,'start':start_datetime,'end':end_datetime}
-    message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
-    mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
-    #console_messages.append(f'<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Requesting WiFi send part file {start_datetime}-{end_datetime}')
-   
-    return redirect('/')
-
-    
-### Całe do wywalenia, główna strona będzie po stronie frontendu, zostanie tylko wiadomosc do plawy do zmiany interwału wiec na razie nie usuwac zeby sie nie zgubilo 
-# @app.route('/',methods=['POST','GET'])
-# def index():
-#     global queued_message
-#     if request.method=="POST":
-#         interval = float(request.form.get('interval'))
-#         py_dict = {"interval":interval}
-#         message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
-#         mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
-#         queued_message = request.form.get("interval")
-#         console_messages.append(f'<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Queued message: {request.form.get("interval")}')
-#         check_length(console_messages)
-#         return render_template('index.html',messages = console_messages, queued_message = queued_message)
-#     return render_template('index.html',messages = console_messages,queued_message = queued_message)
 
 
 
@@ -190,9 +189,21 @@ def index():
     return 'Hello world'
 
 
+# Funkcja do zmiany interwalu - nie zmieniac
+@app.route('/interval_change',methods=['POST'])
+def change_interval():
+    data = request.get_json()
+    if 'interval' in data and isinstance(data['number'], int):
+        interval = data['interval']
+        py_dict = {"interval":interval}
+        message = base64.b64encode(json.dumps(py_dict).encode('utf-8')).decode('utf-8')
+        mqtt.publish('/mqtt/downlink/2cf7f120323086aa',json.dumps({"confirmed": False, "fport": 85, "data": message}))
+        return 200
+    else:
+        return 400
 
 #####
-##### W tym wazne zeby nie ruszac nic poza fragmentami gdzie jest napisane ze mozna
+##### W tym juz lepiej nic nie ruszac powinno byc git
 #####
 @mqtt.on_message()
 def handle_message(client, userdata, message):    
@@ -229,62 +240,41 @@ def handle_message(client, userdata, message):
             )
             db.session.add(data)
             db.session.commit()
-           
+            message = f'<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Data received via LoraWAN: latitude - {latitude}, longitude - {longitude}, memory status - {memory_status}'
 
 
-        ### W tych ifach do zmiany jest logika z message, tutaj trzeba bedzie dawac znac frontendowi ze cos przyszlo - do ustalenia jak to zrobic i co z tym bedzie sie dzialo po stronie frontendu
+        
         elif sign=='I':
             interval = data_parts[1]
-            message = f"Interval changed confirmed - interval: {interval}"
+            message = f"<{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}> Interval change confirmed - interval: {interval}"
 
         elif sign=='P':
             connection = data_parts[1]
             if connection == '1':
-                message = f"5G connection confirmed"
+                message = f"5G connection is available"
             else:
-                message = f"5G connection refused"
+                message = f"5G connection is no longer available"
 
         elif sign=='G':
             connection = data_parts[1]
             if connection == '1':
-                message = f"Whole file sent (5G)"
+                message = f"Data was sent sucessfully"
             else:
-                message = f"Cannot send file"
-        
-        elif sign=='A':
-            connection = data_parts[1]
-            if connection == '1':
-                message = f"Part of file sent (5G)"
-            else:
-                message = f"Cannot send file"
+                message = f"Cannot send data"
 
         elif sign=='B':
             connection = data_parts[1]
             if connection == '1':
-                message = f"WiFi connection confirmed"
+                message = f"WiFi is now on"
             else:
-                message = f"Wifi connection refused"
-
-        elif sign=='C':
-            connection = data_parts[1]
-            if connection == '1':
-                message = f"Whole file sent (Wifi)"
-            else:
-                message = f"Cannot send file"
-        
-        elif sign=='D':
-            connection = data_parts[1]
-            if connection == '1':
-                message = f"Part of file sent (WiFi)"
-            else:
-                message = f"Cannot send file"
+                message = f"WiFi is now off"
 
             
     except KeyError:
         message = "Device connected succesfully"
     
-
+    socketIO.emit('console_message',{'message':message})
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    socketIO.run(app,host='0.0.0.0',debug=True)
